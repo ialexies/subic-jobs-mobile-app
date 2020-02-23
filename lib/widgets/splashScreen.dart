215ee5cd90854eb31ea49cee8e2aa007099b38fe
ialexies <ialexies@gmail.com>
2020-02-23 19:26:11 +0800

    Working google signin and firebase signin

--- lib/widgets/splashScreen.dart
+++ lib/widgets/splashScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+
+
 class SplashScreen extends StatefulWidget {
   // SplashScreen({ Key key, this.duration }) : super(key: key);
 
@@ -24,13 +26,13 @@ class _SplashScreenState extends State<SplashScreen>
     super.initState();
     _controller = AnimationController(
       vsync: this, // the SingleTickerProviderStateMixin
-      duration: Duration(seconds: 10),
+      duration: Duration(seconds: 3),
     );
     _controller.forward();
 
     _controller.addListener(() {
       setState(() {});
-      print(_controller.value);
+      // print(_controller.value);
     });
   }
 
@@ -38,36 +40,36 @@ class _SplashScreenState extends State<SplashScreen>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        color: Colors.red.withOpacity(_controller.value),
+        // color: Colors.red.withOpacity(_controller.value),
         child: Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Flexible(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  widget.image == null ? SizedBox.shrink() : widget.image,
-                  Text(widget.title.toString().toUpperCase(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      )),
-                  SizedBox(height: 15),
-                  Text(widget.description.toString().toUpperCase(),
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.clip,
-                      maxLines: 30,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w100,
-                      )),
-                  widget.content != null ? widget.content : SizedBox.shrink(),
-                ],
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                widget.image == null ? SizedBox.shrink() : widget.image,
+                Text(widget.title.toString().toUpperCase(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black.withOpacity(_controller.value)
+                    )),
+                SizedBox(height: 15),
+                Text(widget.description.toString().toUpperCase(),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.clip,
+                    maxLines: 30,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.black.withOpacity(_controller.value),
+                    )),
+                widget.content != null ? widget.content : SizedBox.shrink(),
+              ],
             ),
           ],
         ),
