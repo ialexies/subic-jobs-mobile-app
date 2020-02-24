342e37bc1972d5175841406fb1379899dfaa66c7
ialexies <ialexies@gmail.com>
2020-02-24 13:05:13 +0800

    testd

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
-import 'package:subicjobs/screens/home.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:subicjobs/widgets/splashScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,8 +8,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: SubicJobs(),
       theme: ThemeData(),
     );
   }
+}
+
+class SubicJobs extends StatefulWidget {
+  @override
+  _SubicJobsState createState() => _SubicJobsState();
+}
+
+class _SubicJobsState extends State<SubicJobs> {
+  bool authStat = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return authStat == false ? unAuthScreen() : authScreen();
+  }
+
+  authScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('dsdsd'),
+      ),
+    );
+  }
+
+  unAuthScreen() {
+    return Scaffold(
+      body: SplashScreen(
+        title: "Please Login",
+        description: "Please Login using your \ngoogle account",
+        content: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: RaisedButton.icon(
+            color: Theme.of(context).primaryColor,
+            label: Text(
+              'Google Login',
+              style: TextStyle(color: Colors.white70),
+            ),
+            icon: Icon(
+              Icons.email,
+              color: Colors.white,
+            ),
+            onPressed: () => print('fdfdf'),
+          ),
+        ),
+        image: Container(
+          child: Column(
+            children: <Widget>[
+              SvgPicture.asset('assets/images/sign_in.svg', height: 160.0),
+              SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
 }
--- lib/widgets/splashScreen.dart
+++ lib/widgets/splashScreen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
-
 class SplashScreen extends StatefulWidget {
   // SplashScreen({ Key key, this.duration }) : super(key: key);
 
@@ -26,13 +24,13 @@ class _SplashScreenState extends State<SplashScreen>
     super.initState();
     _controller = AnimationController(
       vsync: this, // the SingleTickerProviderStateMixin
-      duration: Duration(seconds: 3),
+      duration: Duration(seconds: 10),
     );
     _controller.forward();
 
     _controller.addListener(() {
       setState(() {});
-      // print(_controller.value);
+      print(_controller.value);
     });
   }
 
@@ -40,36 +38,36 @@ class _SplashScreenState extends State<SplashScreen>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        // color: Colors.red.withOpacity(_controller.value),
+        color: Colors.red.withOpacity(_controller.value),
         child: Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                widget.image == null ? SizedBox.shrink() : widget.image,
-                Text(widget.title.toString().toUpperCase(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black.withOpacity(_controller.value)
-                    )),
-                SizedBox(height: 15),
-                Text(widget.description.toString().toUpperCase(),
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.clip,
-                    maxLines: 30,
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w100,
-                      color: Colors.black.withOpacity(_controller.value),
-                    )),
-                widget.content != null ? widget.content : SizedBox.shrink(),
-              ],
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  widget.image == null ? SizedBox.shrink() : widget.image,
+                  Text(widget.title.toString().toUpperCase(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  SizedBox(height: 15),
+                  Text(widget.description.toString().toUpperCase(),
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.clip,
+                      maxLines: 30,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w100,
+                      )),
+                  widget.content != null ? widget.content : SizedBox.shrink(),
+                ],
+              ),
             ),
           ],
         ),
